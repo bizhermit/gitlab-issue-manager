5b289b0f6f9d22a0722df3757f08166be9e3cefc
bizhermit <rsenda@bizhermit.com>
2022-02-16 22:02:52 +0900

    fetch git / iframe

--- src/modules/fetch-git.ts
+++ src/modules/fetch-git.ts
@@ -7,3 +7,23 @@ const fetchGit = async <T = Struct>(gitCtx: GitAccountProps, url: string, option
 };
 
 export default fetchGit;
+
+export const getGitUser = (gitCtx: GitAccountProps) => {
+    return fetchGit<Struct>(gitCtx, `user`);
+};
+
+export const getGitProjects = (gitCtx: GitAccountProps, options?: {}) => {
+    return fetchGit<Array<Struct>>(gitCtx, "projects?state=opened&per_page=100");
+};
+
+export const getProject = (gitCtx: GitAccountProps, projectId: number, options?: {}) => {
+    return fetchGit<Struct>(gitCtx, `projects/${projectId}`);
+};
+
+export const getIssues = (gitCtx: GitAccountProps, options?: { projectId?: number; includeClosed?: boolean; }) => {
+    return fetchGit<Array<Struct>>(gitCtx, `projects/${options?.projectId}/issues?${options?.includeClosed ? "" : "state=opened"}`);
+};
+
+export const getIssue = (gitCtx: GitAccountProps, projectId: number, issueId: number, options?: {}) => {
+    return fetchGit<Struct>(gitCtx, `projects/${projectId}/issues/${issueId}`);
+};
